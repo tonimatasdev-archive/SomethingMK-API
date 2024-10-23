5b2ae805f640bb24c44630222f52580cdb603700
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-10-23 22:49:34 +0200

    Add JDA to the bot class

--- src/main/java/dev/tonimatas/discordmk/Bot.java
+++ src/main/java/dev/tonimatas/discordmk/Bot.java
@@ -2,19 +2,28 @@ package dev.tonimatas.discordmk;
 
 import dev.tonimatas.discordmk.blocks.Block;
 import dev.tonimatas.discordmk.blocks.StaticBlock;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Bot {
-    public String id;
-    public String token;
-    public List<Block> blocks;
-    public List<StaticBlock> staticBlocks;
+    public JDA jda;
+    public final String id;
+    public final String token;
+    public final List<Block> blocks;
+    public final List<StaticBlock> staticBlocks;
     
     public Bot(String id, String token, List<Block> blocks, List<StaticBlock> staticBlocks) {
         this.id = id;
         this.token = token;
         this.blocks = blocks;
         this.staticBlocks = staticBlocks;
+    }
+    
+    public void start() {
+        this.jda = JDABuilder.create(token, Arrays.asList(GatewayIntent.values())).build();
     }
 }
