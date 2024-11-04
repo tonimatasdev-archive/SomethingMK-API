fd646771016a0320cb38bbcda0720af248ed47b3
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2024-11-05 00:09:32 +0100

    Use lombok

--- src/main/java/dev/tonimatas/discordmk/blocks/types/AbstractBlock.java
+++ src/main/java/dev/tonimatas/discordmk/blocks/types/AbstractBlock.java
@@ -2,25 +2,14 @@ package dev.tonimatas.discordmk.blocks.types;
 
 import dev.tonimatas.discordmk.Action;
 import dev.tonimatas.discordmk.blocks.Block;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.UUID;
 
+@Getter
+@RequiredArgsConstructor
 public abstract class AbstractBlock implements Block {
-    public UUID id;
-    public Action action;
-    
-    public AbstractBlock(Action action) {
-        this.id = UUID.randomUUID();
-        this.action = action;
-    }
-    
-    @Override
-    public UUID getId() {
-        return id;
-    }
-
-    @Override
-    public Action getAction() {
-        return action;
-    }
+    protected final UUID id = UUID.randomUUID();
+    public final Action action;
 }
